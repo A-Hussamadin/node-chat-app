adcfd5f13506b4aa59c2c26cdf7e1d7f0468e544
A-Hussamadin <h.abushelha@gmail.com>
2018-08-05 01:42:47 +0300

    adding Heroku process

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
 app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log("Server Started on Port 3000");
+app.listen(port, () => {
+    console.log(`Server Started on Port: ${port}`);
 });
